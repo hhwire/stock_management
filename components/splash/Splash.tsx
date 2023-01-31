d739e478a50bacee8351e2ae8edc7daee0dac282
Kiang Sornrasak <sronrasak@gmail.com>
2023-01-31 22:16:35 +0700

    docker

--- components/splash/Splash.tsx
+++ components/splash/Splash.tsx
@@ -42,6 +42,7 @@ export default function Splash() {
           <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
             <h1 className="my-4 text-xl md:text-7xl text-indigo-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">
               Main Hero message to sell your app
+              asdasd
             </h1>
             <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">
               Sub-hero message, not too long and not too short. Make it just
